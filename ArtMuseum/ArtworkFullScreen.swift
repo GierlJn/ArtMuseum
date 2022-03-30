ef808ce363ba2423b9493c47528fee460b07ffa0
Julian <julian.gierl@gmx.de>
2022-03-30 11:08:11 +0300

    Added list to fullscreen

--- ArtMuseum/ArtworkFullScreen.swift
+++ ArtMuseum/ArtworkFullScreen.swift
@@ -9,11 +9,39 @@ import SwiftUI
 
 struct ArtworkFullScreen: View {
   var artWork = MockData.mockArtWork(id: 1)
+  var relatedArtworks = MockData.mockArtWorks
     var body: some View {
-      Image(uiImage: artWork.image)
-        .resizable()
-        .aspectRatio(contentMode: .fill)
-        .ignoresSafeArea(.all, edges: .all)
+      
+        
+        
+        VStack(alignment: .leading) {
+          Spacer()
+          Text(artWork.title)
+            .foregroundColor(.white)
+            .bold()
+            .lineLimit(2)
+            .font(.title)
+            .padding()
+          ScrollView(.horizontal, showsIndicators: false){
+            HStack{
+              ForEach(relatedArtworks, id: \.id){ singleArtwork in
+                Image(uiImage: singleArtwork.image)
+                  .resizable()
+                  .frame(width: 180, height: 240)
+                  .clipShape(RoundedRectangle(cornerRadius: 24))
+                  .padding(.horizontal, 4)
+              }
+            }
+          }
+        }
+        .background{
+          Image(uiImage: artWork.image)
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .ignoresSafeArea()
+        }
+      
+      
     }
 }
 
