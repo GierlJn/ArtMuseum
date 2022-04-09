97e90f6328cb7b9eb9cec3ebacabeb615be2c5a3
Julian <julian.gierl@gmx.de>
2022-04-09 19:20:55 +0200

    Artist struct added

--- ArtMuseum/Model/Artist.swift
+++ ArtMuseum/Model/Artist.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+
+struct Artist: Codable, Identifiable{
+  let id: Int
+  let title: String?
+  let sortTitle: String?
+  let artworkIds: String?
+  let birthDate: String?
+  let deathDate: String?
+  
+  var wrappedTitle: String{
+    title ?? "N/A"
+  }
+  
+  var wrappedSortTitle: String{
+    sortTitle ?? "N/A"
+  }
+  
+  var wrappedArtworkIds: String{
+    artworkIds ?? "N/A"
+  }
+  
+  var wrappedBirthDate: String{
+    birthDate ?? "N/A"
+  }
+  
+  var wrappedDeathDate: String{
+    deathDate ?? "N/A"
+  }
+}
